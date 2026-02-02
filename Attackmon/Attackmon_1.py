67f16355841da5fce01be82aabe648a33154c17f
Langer <Tillmann.Langer@ukm-gruppe.com>
2026-02-02 08:48:43 +0100

    pferd

---
+++ Attackmon/Attackmon_1.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+try:
+    with sqlite3.connect("Attackmon.db") as conn:
+        print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+
+except sqlite3.OperationalError as e:
+    print("Failed to open database:", e)
+
+cursor = conn.cursor()
