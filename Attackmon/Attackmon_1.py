19fcab1780431588c11f7e16313c451674179096
Langer <Tillmann.Langer@ukm-gruppe.com>
2026-02-03 15:06:21 +0100

    Localhost now with Bisasam attacks

--- Attackmon/Attackmon_1.py
+++ Attackmon/Attackmon_1.py
@@ -1,26 +1,45 @@
 import sqlite3
-import fastapi
+from fastapi import FastAPI
+'''
+conn = sqlite3.connect("Attackmon.db")
+print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+cursor = conn.cursor()
+cursor.execute("""
+INSERT OR IGNORE INTO ZT_LVL (P_NR, ATT_NR, Level)
+VALUES
+    (1,33,1),
+    (1,45,1),
+    (1,73,7),
+    (1,22,13),
+    (1,77,20),
+    (1,75,27),
+    (1,74,34),
+    (1,79,41),
+    (1,76,48);
+""")'''
+
+#conn.commit()
+#conn.close()
 
 try:
-    with sqlite3.connect("Attackmon.db") as conn:
-        print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
-        cursor = conn.cursor()
-        #cursor.execute('''CREATE TABLE IF NOT EXISTS Level_Attacke (
-        #ATT_NR INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        #Name TEXT NOT NULL,
-        #Typ TEXT NOT NULL,
-        #Schadentyp TEXT NOT NULL,
-        #Attackenschaden TEXT NOT NULL,
-        #Genauigkeit TEXT NOT NULL);''')
-        #conn.commit()
-        #print("Table created successfully.")
+    #with sqlite3.connect("Attackmon.db") as conn:
+        #print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+        #cursor = conn.cursor()
 
+        app = FastAPI()
 
-        cursor.execute('''SELECT * FROM Level_Attacke''',)
-        all_pokemon = cursor.fetchall()
-        for pokemon in all_pokemon:
-            print(pokemon)
-
+        @app.get("/")
+        async def root():
+            conn = sqlite3.connect("Attackmon.db")
+            print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+            cursor = conn.cursor()
+            cursor.execute('''SELECT Pokemon.Name,Level_Attacke.Name,ZT_LVL.Level  FROM ZT_LVL
+            JOIN Pokemon ON ZT_LVL.P_NR = Pokemon.P_NR
+            JOIN Level_Attacke ON ZT_LVL.ATT_NR = Level_Attacke.ATT_NR''',)
+            all_attacken = cursor.fetchall()
+            #for attacke in all_attacken:
+                #print(attacke)'
+            return all_attacken
 
 
 except sqlite3.OperationalError as e:
